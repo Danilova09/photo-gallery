2dc3c4b98f55e0d15fda299f17e9d14473b9af1d
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2022-04-09 16:49:01 +0600

    Added fixtures

--- api/fixtures.js
+++ api/fixtures.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const config = require("./config");
 const User = require("./models/User");
+const Photo = require("./models/Photo");
 
 const run = async () => {
     await mongoose.connect(config.mongo.db, config.mongo.options);
@@ -11,7 +12,7 @@ const run = async () => {
         await mongoose.connection.db.dropCollection(coll.name);
     }
 
-    await User.create({
+    const [USER, ADMIN] = await User.create({
         avatar: null,
         displayName: 'user',
         email: 'user@gmail.com',
@@ -26,6 +27,40 @@ const run = async () => {
         token: 'admin',
         role: 'admin'
     });
+
+    await Photo.create({
+        user: USER,
+        title: 'Mountains',
+        image: 'mountains.jpg',
+    }, {
+        user: USER,
+        title: 'Nature',
+        image: 'nature.jpg',
+    }, {
+        user: USER,
+        title: 'Memories',
+        image: 'memories.jpg',
+    }, {
+        user: USER,
+        title: 'Sunset',
+        image: 'sunset.jpg',
+    }, {
+        user: ADMIN,
+        title: 'Sea',
+        image: 'sea.jpg',
+    }, {
+        user: ADMIN,
+        title: 'Lake',
+        image: 'lake.jpg',
+    }, {
+        user: ADMIN,
+        title: 'Forest',
+        image: 'forest.jpg',
+    }, {
+        user: ADMIN,
+        title: 'Weekend',
+        image: 'weekend.jpg',
+    })
 
     await mongoose.connection.close();
 }
